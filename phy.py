4753ce30436f99d432b70ef7a208f129c279ece7
Inglorion35 <inglorion@home.com>
2021-01-11 15:42:25 +0530

    dummy commit before merge

--- phy.py
+++ phy.py
@@ -1,20 +1,23 @@
 import math
+import pygame
 
 def PlanePhy(self, liftc, dragc, gravity, HEIGHT):
   
   if self.rect.y < HEIGHT-self.rect.height:
       self.vel.y+=gravity
-
+      
   wingarea=(math.cos(self.angle))
   dragarea=(math.sin(self.angle))
 
-  drag = pygame.Vector2(x,y)
-
   lift = liftc*(self.vel.y**2)*wingarea
-  d=dragc*(self.vel.magnitude_squared())
+  d=dragc*(self.vel.magnitude_squared())*dragarea
   drag = pygame.math.Vector2(d*self.vel.x,d*self.vel.y)
   # drag = self.vel.normalize()*self.vel.magnitude_squared()*dragc
-  if self.vel.magnitude()>0:
-    self.vel = self.vel-drag
+  
+  if (self.vel.magnitude()-drag.magnitude()) > 0:
+    self.vel = self.vel - drag
+
+  
+
   if self.rect.y>self.rect.height:
     self.vel.y=(self.vel.y)-lift
